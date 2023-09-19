8dcbee812e27881b15f1d31e740db769b1a00b8f
Laxman Giri <kesharigiri000@gmail.com>
2023-09-19 10:48:54 +0545

    u[date validation

--- api/controllers/users.go
+++ api/controllers/users.go
@@ -1,19 +1,16 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/Girilaxman000/go-gin/api/dtos"
 	"github.com/Girilaxman000/go-gin/api/models"
 	"github.com/Girilaxman000/go-gin/api/services"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func UsersCreate(ctx *gin.Context) {
 	var users models.User
-	fmt.Println(users)
 	err := ctx.BindJSON(&users)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
@@ -22,15 +19,13 @@ func UsersCreate(ctx *gin.Context) {
 		return
 	}
 
-	validate := validator.New()
-	errVal := validate.Struct(users)
+	errVal := models.ValidateUser(users)
 	if errVal != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": errVal,
+			"error": errVal.Error(),
 		})
 		return
 	}
-
 	err = services.UsersCreate(users)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
--- api/models/users.go
+++ api/models/users.go
@@ -3,6 +3,7 @@ package models
 import (
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
@@ -14,6 +15,18 @@ type User struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+// Create a global validator instance
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
+// ValidateUser validates the User struct
+func ValidateUser(user User) error {
+	return validate.Struct(user)
 }
 
 // IsAdmin  bool   `gorm:"default:false" json:"isAdmin"`
